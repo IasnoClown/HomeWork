1a817a782417df9d360e3a1ce50321b2676eb701
IasnoClown <mrjlekc466@gmail.com>
2024-12-06 13:04:15 +0500

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,11 +1,14 @@
 # Задача "Нули ничто, отрицание недопустимо!"
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-posit = 0
-while posit < len(my_list):
-    if my_list[posit] < 0:
-        posit += 1
+i = 0
+while i < len(my_list):
+    if my_list[i] == 99:
+        print(my_list[i])
         break
-    print(my_list[posit])
-    posit += 1
+    elif my_list[i] <= 0:
+        i += 1
+        continue
+    print(my_list[i])
+    i += 1
 else:
     print('Положительные числа закончились')
